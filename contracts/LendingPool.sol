0c44a0a348c1b8a90b910fa67b630a2906071148
Genexa <kanjuttai1@gmail.com>
2026-03-12 19:25:23 +0700

    Create LendingPool.sol

---
+++ contracts/LendingPool.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract LendingPool {
+
+    mapping(address => uint256) public deposits;
+    mapping(address => uint256) public borrows;
+
+    function deposit() external payable {
+        deposits[msg.sender] += msg.value;
+    }
+
+    function borrow(uint256 amount) external {
+        require(deposits[msg.sender] >= amount * 2, "Not enough collateral");
+
+        borrows[msg.sender] += amount;
+        payable(msg.sender).transfer(amount);
+    }
+
+    function repay() external payable {
+        borrows[msg.sender] -= msg.value;
+    }
+
+}
