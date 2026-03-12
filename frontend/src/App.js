59031c010d057081181e9f4023f5da5e3e4f92b6
Genexa <kanjuttai1@gmail.com>
2026-03-12 19:27:30 +0700

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import Deposit from "./components/Deposit";
+import Borrow from "./components/Borrow";
+
+function App() {
+
+  return (
+    <div>
+
+      <h1>Genexa Lending Dashboard</h1>
+
+      <Deposit />
+      <Borrow />
+
+    </div>
+  );
+
+}
+
+export default App;
